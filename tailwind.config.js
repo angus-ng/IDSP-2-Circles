453c115c2f48a6946ea5cc287c064d891424fd4c
achen930 <annabellechen101@gmail.com>
2024-04-30 16:52:55 -0700

    Add lower nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         'light-mode-bg': 'rgba(248, 244, 234, 0.92)',
         'gradient-lighter-blue': 'rgba(0, 66, 200, 0.5)',
         'gradient-medium-blue': '#04318b',
-        'gradient-darker-blue': 'rgba(12, 18, 28, 0.5)'
+        'gradient-darker-blue': 'rgba(12, 18, 28, 0.5)',
+        'grey': '#3A3A3A'
       },
       fontSize: {
         'h1': '29px',
@@ -20,7 +21,8 @@ module.exports = {
         'body': '17px',
         'secondary': '12px',
         'tertiary': '10px',
-        '15': '15px'
+        '15': '15px',
+        '13': '13px'
       },
       lineHeight: {
         'h1': '40px',
