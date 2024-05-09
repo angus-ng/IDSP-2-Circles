072d89803dc579acb36959caee1630070db0eaa2
Angus Ng <ang183@my.bcit.ca>
2024-05-09 11:58:50 -0700

    update middleware

--- src/middleware/express.middlewares.ts
+++ src/middleware/express.middlewares.ts
@@ -6,9 +6,7 @@ import Redis from "ioredis";
 const RedisStore = require("connect-redis").default
 
 module.exports = (app : Application) => {
-  app.set("views", path.join(__dirname, "..", "areas"));
-  app.set('view engine', 'ejs');
-  app.use(express.static(path.join(__dirname, "..", "public")));
+  app.use(express.static("public"));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(morgan("tiny"));
