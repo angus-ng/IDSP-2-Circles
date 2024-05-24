edffb7ec0433c9d2e445b0d0ea8fbf1567d73aa3
Angus Ng <ang183@my.bcit.ca>
2024-05-23 18:40:20 -0700

    fix google maps to use one map, clears markers and reloads markers

--- public/map.js
+++ public/map.js
@@ -1,12 +1,13 @@
 let map;
+let storedMap;
 let markers = [];
+let scriptImported = false;
 
 async function initMap() {
   const { Map } =  await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
   try { 
-    await clearMarkers()
     const data = await getMapInfo();
     const newestAlbum = data.data.Album[0]
     let lat = 0
@@ -15,12 +16,29 @@ async function initMap() {
       lat = parseFloat(newestAlbum.lat)
       long = parseFloat(newestAlbum.long)
     }
+    if (!map){
     map = new Map(document.getElementById("map"), {
       center: { lat: lat, lng: long },
       zoom: 8,
       mapId: "circles-421907",
     });
-    for (let album of data.data.Album) {
+  }
+    map.deleteMarkers = function deleteMarkers() {
+      clearMarkers();
+      markers = [];
+    }
+    
+    function clearMarkers() {
+      setMapOnAll(null);
+    }
+    
+    function setMapOnAll(map) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+    }
+    map.createAlbumMarkers = (albumData) => {
+      for (let album of albumData.data.Album) {
         console.log(album);
         const contentNode = document.createElement("div");
         contentNode.classList.add("mapAlbum");
@@ -28,7 +46,6 @@ async function initMap() {
             <img src="${album.photos[0].src}" class="rounded-full border-4 border-blue-500 w-12 h-12 object-cover aspect-w-1 aspect-h-1" alt="Google Maps Pin">
             <div class="absolute w-4 h-4 bg-blue-500 rounded-full bottom-0 left-1/2 transform -translate-x-1/2"></div>
         </div>`;
-    
         const marker = new AdvancedMarkerElement({
             map,
             position: {lat: parseFloat(album.lat), lng: parseFloat(album.long)},
@@ -37,6 +54,9 @@ async function initMap() {
         });
         markers.push(marker)
       }
+    }
+    map.createAlbumMarkers(data)
+      storedMap = document.querySelector("#map")
   } catch (error) {
     console.log(error)
   }
@@ -60,40 +80,56 @@ async function displayMap() {
       pageName.textContent = "Maps"
       leftHeaderButton.innerHTML = `<img src="/lightmode/back_button.svg" alt="Back Button" id="mapBackButton"/>`;
       rightHeaderButton.innerHTML = "";
-  
-      const mapDiv = document.createElement("div")
-      mapDiv.id = "map"
-      mapDiv.classList.add("h-full")
-      pageContent.innerHTML = ""
-      console.log(pageContent.innerHTML)
-      pageContent.appendChild(mapDiv) 
-  
-      const mapScript = document.createElement("script")
-      mapScript.async = true
-      mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapKey}&loading=async&callback=initMap`
-      pageContent.appendChild(mapScript)
+      
+      if (storedMap) {
+        map.deleteMarkers()
+        const data = await getMapInfo();
+        map.createAlbumMarkers(data)
+        pageContent.innerHTML = ""
+        console.log("MAP", storedMap)
+        console.log(markers)
+        pageContent.appendChild(storedMap)
+      } else {
+        const mapDiv = document.createElement("div")
+        mapDiv.id = "map"
+        mapDiv.classList.add("h-full")
+        pageContent.innerHTML = ""
+        console.log(pageContent.innerHTML)
+        pageContent.appendChild(mapDiv) 
+      }
+      if (!scriptImported){
+        const mapScript = document.createElement("script")
+        mapScript.async = true
+        mapScript.src = `https://maps.googleapis.com/maps/api/js?v=beta&key=${googleMapKey}&loading=async&callback=initMap`
+        mapScript.defer = true
+        pageContent.append(mapScript)
+        scriptImported = true
+      }
     } catch (error) {
       console.log(error)
     }
 }
 
 
-async function clearMarkers() {
-  reload = false
-  markers.forEach(marker => {
-    console.log(marker)
-    console.log(marker.map)
-    reload = true
-    // this is suppose to get rid of marker....
-    marker.map = null;
-  });
-  if (reload) {
-    // get rid of this location.reload 
-    // right now theres a problem where if i call the map after calling it once
-    // the markers wont be placed again because they've been made before and remembered on the page
-    // refreshing works but its just not ideal.
-    location.reload(true);
-    await displayMap()
-  }
-  markers = [];
-}
+// async function clearMarkers() {
+//   reload = false
+//   for (let i = 0; i < markers.length; i++) {
+//     markers[i].setMap(null);
+//   }
+//   // markers.forEach(marker => {
+//   //   console.log(marker)
+//   //   console.log(marker.map)
+//   //   reload = true
+//   //   // this is suppose to get rid of marker....
+//   //   marker.map = null;
+//   // });
+//   if (reload) {
+//     // get rid of this location.reload 
+//     // right now theres a problem where if i call the map after calling it once
+//     // the markers wont be placed again because they've been made before and remembered on the page
+//     // refreshing works but its just not ideal.
+//     location.reload(true);
+//     await displayMap()
+//   }
+//   markers = [];
+// }
